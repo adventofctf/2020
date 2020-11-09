50fdb76c34370f64c9fee76652534ebd8f5d03d5
Arjen Wiersma <arjen@wiersma.org>
2020-11-09 21:42:56 +0100

    Start, install and sync scripts for challenges

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "=== Starting ctfd ==="
-cd CTFd && docker-compose up -d > start.logs && cd ..
-
 echo "=== Starting challenges ==="
 for i in `find . -name 'challenge.yml' 2>/dev/null`; do
     dir=$(dirname $i | cut -d '/' -f 2)
     echo " [*] Starting $dir"
     cd $dir
-    docker-compose up -d
+    ctf challenge install
     cd -
 done
