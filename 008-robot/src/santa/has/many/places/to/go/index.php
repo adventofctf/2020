a68763af14cc1b12a6d27efac376f769f29269c0
Arjen Wiersma <arjen@wiersma.org>
2020-11-12 20:57:53 +0100

    Nr 8, using robots.txt to find a place to go.

---
+++ 008-robot/src/santa/has/many/places/to/go/index.php
@@ -0,0 +1,45 @@
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Advent of CTF 8</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="/style.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div class="jumbotron bg-transparent mb-0 radius-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 mx-auto">
+                        <h1 class="display-2">Advent of CTF <span class="vim-caret">8</span></h1>
+                        <div class="lead mb-3 text-mono text-warning">Your daily dose of CTF for December</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6 mx-auto"
+                <div class="card text-center">
+                    <div class="card-header">
+                        Fun facts
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            NOVI{you_have_br@1ns_in_your_head}
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        Solved by you.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
