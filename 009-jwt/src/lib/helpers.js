b740c78d127a3bd1fae371bd9ae9e69f6db73311
Arjen Wiersma <arjen@wiersma.org>
2020-11-17 21:23:02 +0100

    Challenge 9 JWT

---
+++ 009-jwt/src/lib/helpers.js
@@ -0,0 +1,206 @@
+var Cookies = require('cookies')
+var qs   = require('querystring');
+var fs   = require('fs');
+var path = require('path');
+
+var level = require('level');
+var db = level(__dirname + '/db');
+
+var jwt  = require('jsonwebtoken');
+var secret = process.env.JWT_SECRET || "ADVENT_OF_CTF_2020"; // super secret
+
+function loadView(view) {
+  var filepath = path.resolve(__dirname, '../views/', view + '.html');
+  return fs.readFileSync(filepath).toString();
+}
+
+// Content
+var index      = loadView('index');      // default page
+var restricted = loadView('restricted'); // only show if JWT valid
+var admin      = loadView('admin');      // only show if JWT valid and the role is admin
+var fail       = loadView('fail');       // auth fail
+
+// show fail page (login)
+function authFail(res, callback) {
+  res.writeHead(401, {'content-type': 'text/html'});
+  return res.end(fail);
+}
+
+// generate a GUID
+function generateGUID() {
+  return new Date().getTime(); // we can do better with crypto
+}
+
+// create JWT
+function generateToken(req, GUID, opts, role) {
+  opts = opts || {};
+
+  // By default, expire the token after 7 days.
+  // NOTE: the value for 'exp' needs to be in seconds since
+  // the epoch as per the spec!
+  var expiresDefault = '7d';
+
+  var token = jwt.sign({
+    auth:  GUID,
+    text: "I do love a good puzzle.",
+    role: role
+  }, secret, { expiresIn: opts.expires || expiresDefault });
+
+  return token;
+}
+
+function generateAndStoreToken(req, opts, role) {
+  var GUID   = generateGUID(); // write/use a better GUID generator in practice
+  var token  = generateToken(req, GUID, opts, role);
+  var record = {
+    "valid" : true,
+    "created" : new Date().getTime()
+  };
+
+  db.put(GUID, JSON.stringify(record), function (err) {
+    // console.log("record saved ", record);
+  });
+
+  return token;
+}
+
+function authSuccess(req, res, role) {
+  var token = generateAndStoreToken(req, null, role);
+
+  var cookies = new Cookies(req, res)
+  cookies.set('token', token)
+  res.writeHead(302, {
+    'Location': '/admin'
+  });
+  return res.end();
+}
+
+// lookup person in "database"
+var u = { un: 'user', pw: 'incorrect', role: 'user' };
+
+// handle authorisation requests
+function authHandler(req, res){
+  if (req.method === 'POST') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+    }).on('end', function () {
+      var post = qs.parse(body);
+      if(post.username && post.username === u.un && post.password && post.password === u.pw) {
+        return authSuccess(req, res, u.role);
+      } else {
+        return authFail(res);
+      }
+    });
+  } else {
+    return authFail(res);
+  }
+}
+
+function verify(token) {
+  var decoded = false;
+  jwt.verify(token, secret, function (err, payload) {
+    if (err) {
+      decoded = false; // still false
+    } else {
+      decoded = payload;
+    }
+  });
+  return decoded;
+}
+
+function administrator(res, token, role) {
+  res.writeHead(200, {
+    'content-type': 'text/html'
+  });
+  if (role === 'user') {
+    return res.end(restricted);
+  } else {
+    return res.end(admin);
+  }
+}
+
+function validate(req, res, callback) {
+  var cookies = new Cookies(req, res)
+  var token = cookies.get('token')
+  var decoded = verify(token);
+  if(!decoded || !decoded.auth) {
+    authFail(res);
+    return callback(res);
+  } else {
+    db.get(decoded.auth, function (err, record) {
+      var r;
+      try {
+        r = JSON.parse(record);
+      } catch (e) {
+        r = { valid : false };
+      }
+      if (err || !r.valid) {
+        authFail(res);
+        return callback(res);
+      } else {
+        administrator(res, token, decoded.role);
+        return callback(res);
+      }
+    });
+  }
+}
+
+function notFound(res) {
+  res.writeHead(404, {'content-type': 'text/plain'});
+  return res.end('404 Not Found');
+}
+
+function home(res) {
+  res.writeHead(200, {'content-type': 'text/html'});
+  return res.end(index);
+}
+
+function done(res) {
+  return; // does nothing. (pass as callback)
+}
+
+function logout(req, res, callback) {
+  // invalidate the token
+  var cookies = new Cookies(req, res);
+  var token = cookies.get('token');
+  // console.log(' >>> ', token)
+  var decoded = verify(token);
+  if(decoded) { // otherwise someone can force the server to crash by sending a bad token!
+    // asynchronously read and invalidate
+    db.get(decoded.auth, function(err, record) {
+      if (!err) {
+        var updated    = JSON.parse(record);
+        updated.valid  = false;
+        db.put(decoded.auth, updated, function (err) {
+          // console.log('updated: ', updated)
+        });
+      }
+      cookies.set('token');
+      res.writeHead(302, {
+        'Location': '/'
+      });
+      res.end();
+      return callback(res);
+    });
+  } else {
+    cookies.set('token');
+    authFail(res, done);
+    return callback(res);
+  }
+}
+
+
+module.exports = {
+  fail : authFail,
+  done: done, // moch callback
+  home: home,
+  handler : authHandler,
+  logout : logout,
+  notFound : notFound,
+  success : authSuccess,
+  validate : validate,
+  verify : verify,
+  view : loadView,
+  generateAndStoreToken: generateAndStoreToken
+}
---
+++ 009-jwt/src/server.js
@@ -0,0 +1,28 @@
+var port = 8080;
+var http = require('http');          // core node.js http (no frameworks)
+var url = require('url');            // core node.js url (no frameworks)
+var app  = require('./lib/helpers'); // auth, token verification & render helpers
+var c    = function(res){ /*  */ };
+var mpath = require('path');
+var fs = require('fs');
+
+process.on('SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  process.exit(1);
+});
+
+http.createServer(function (req, res) {
+  var path = url.parse(req.url).pathname;
+  if( path === '/' || path === '/home' ) { app.home(res);           } // homepage
+  else if( path === '/auth')    { app.handler(req, res);            } // authenticator
+  else if( path === '/admin') { app.validate(req, res, app.done); } // private content
+  else if( path === '/logout')  { app.logout(req, res, app.done);   } // end session
+  else if(req.url.match("\.css$")){
+    var cssPath = mpath.join(__dirname, 'public', req.url);
+    var fileStream = fs.createReadStream(cssPath, "UTF-8");
+    res.writeHead(200, {"Content-Type": "text/css"});
+    fileStream.pipe(res) }
+  else                          { app.notFound(res);                } // 404 error
+}).listen(port);
+
+console.log("The server is running at " + port);
