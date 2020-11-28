95ddd41dc89d65c2e41672284c4e1d2aaa1eda2b
Arjen Wiersma <arjen@wiersma.org>
2020-11-28 14:28:09 +0100

    :wrench: Add first 8 badges, update source

--- badge/src/badge.php
+++ badge/src/badge.php
@@ -12,20 +12,31 @@ $points = 0;
 
 if ($key) {
     $data = [
-        "3f12301d8715a1371d2d776d25ea6ab6" => [
-            "solve" => 1,
-            "points" => 100
-        ],
-
-        "4a2932632b3eb256e44cf9b0728911b4" => [
-            "solve" => 4,
-            "points" => 400
-        ],
-
-        "c366d63edd4a35c9f8bea89e57401fef" => [
-            "solve" => 6,
-            "points" => 600
-        ]
+        "f90b2bf3f08ee628c09505ff309018ed" => ["solve" => 0, "points"=>1],
+        "3f12301d8715a1371d2d776d25ea6ab6" => ["solve" => 1, "points"=>100],
+        "948b1eb046c96865a05808660ee99e10" => ["solve" => 2, "points"=>200],
+        "4a2932632b3eb256e44cf9b0728911b4" => ["solve" => 3, "points"=>300],
+        "f1d6ca5572e0c012239bcf4a8f797be1" => ["solve" => 4, "points"=>400],
+        "080b5d5fcaf13167d2e7e8871fdc8ded" => ["solve" => 5, "points"=>500],
+        "c366d63edd4a35c9f8bea89e57401fef" => ["solve" => 6, "points"=>600],
+        "9fac6046540f4972c60f458b94aacb1d" => ["solve" => 7, "points"=>700],
+        "da781419d6bf02d0a580e48414b9cbde" => ["solve" => 8, "points"=>800],
+        "973ded4b2381c28af6c24d3d670303c6" => ["solve" => 9, "points"=>900],
+        "ba15475608ea3f8313825eec5dceac06" => ["solve" => 10, "points"=>1000],
+        "3542630bd0bb5141d94e4b40930bd69d" => ["solve" => 11, "points"=>1100],
+        "af3424cd215a6459494ae07eab33cb35" => ["solve" => 12, "points"=>1200],
+        "24e9ce8f146f70b4189f1d2532a75208" => ["solve" => 13, "points"=>1300],
+        "dd04640480d764ab09eea047cde749cd" => ["solve" => 14, "points"=>1400],
+        "9c6afd807a15973b962cf3aee3dbe836" => ["solve" => 15, "points"=>1500],
+        "246397ca184f8b03ac8fecf50ee1051e" => ["solve" => 16, "points"=>1600],
+        "8717d728f2de96beb8123c0cca28a728" => ["solve" => 17, "points"=>1700],
+        "be40bcd25e7487440a64b13cd32049b2" => ["solve" => 18, "points"=>1800],
+        "d80f13d1ab714f7864c2a9ef56c5f767" => ["solve" => 19, "points"=>1900],
+        "c1f93b6ee2e1cd25ea02f9a78c364b12" => ["solve" => 20, "points"=>2000],
+        "a4afd1fffb0b662d849a6907767f0625" => ["solve" => 21, "points"=>2100],
+        "6c0810c1568645bcf58da67a1db6e3e7" => ["solve" => 22, "points"=>2200],
+        "497784f7a3314f8aa5b8464432e30bbe" => ["solve" => 23, "points"=>2300],
+        "b915cb528c4b3d6fc4644f73ba8b829d" => ["solve" => 24, "points"=>2400]
     ];
 
     $entry = $data[$key];
@@ -114,16 +125,17 @@ if ($key) {
                     <img class="card-img-top" src="/badges/<?= $key ?>.png" alt="Card image cap">
                     <div class="card-body">
                         <h4 class="card-title">You solved challenge <?= $solve ?>!</h4>
-                        <p class="card-text">Congratulations, you earned it. Be sure to share this badge on social media! There is nothing like boasting about finishing a challenge before your friends did!</p>
                         <?php
                         if (!$_GET["share"]) {
                         ?>
+                            <p class="card-text">Congratulations, you earned it. Be sure to share this badge on social media! There is nothing like boasting about finishing a challenge before your friends did!</p>
                             <a href="https://twitter.com/intent/tweet?text=I just solved challenge <?= $solve ?> on Advent of CTF @adventofctf&url=https%3A%2F%2Fbadges%2Eadventofctf%2Ecom%2Fbadges%2Ephp%3Fkey%3D<?= $key ?>%26share=1&hashtags=ctf,advent,hack" class="btn btn-info" target=_blank><span class="fab fa-twitter mr-2"></span> Share on twitter</a>
                             <a class="btn btn-primary" onclick="myCopy()" onmouseout="outFunc()"><span class="far fa-copy mr-2"></span> <span class="tooltiptext" id="myTooltip">Copy share URL</span></a>
 
                         <?php
                         } else {
                         ?>
+                            <p class="card-text">There is nothing like boasting about finishing a challenge before your friends did!</p>
                             <a href="https://www.adventofctf.com" class="btn btn-warning"><span class="fas fa-play mr-2"></span> Join the fun, play the Advent of CTF</a>
                         <?php
                         }
@@ -136,7 +148,7 @@ if ($key) {
         <script type="text/javascript">
          function myCopy() {
              /* Get the text field */
-             var theText = "https://badges.adventofctf.com/badge.php?key=4a2932632b3eb256e44cf9b0728911b4&share=1" ;
+             var theText = "https://badges.adventofctf.com/badge.php?key=<?=$key?>&share=1" ;
              var hiddenCopy = document.createElement('div');
              //set the innerHTML of the div
              hiddenCopy.innerHTML = theText;
